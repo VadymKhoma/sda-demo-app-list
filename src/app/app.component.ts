bbd5ff059240f6aa507624efc8a7bfb9142d9811
Szymon Rybka <szymon@MacBook-Pro-Szymon.local>
2018-03-21 20:33:23 +0100

    Moved list to view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,5 @@ import { CartItem } from './model/cart-item';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public telephones: CartItem[] = [
-    {
-      name: 'samsung s9+',
-      price: 300,
-    },
-    {
-      name: 'lg v30',
-      price: 301,
-    }
-  ];
 
-  public itStuff: CartItem[] = [
-    {
-      name: 'magic mouse',
-      price: 200,
-    },
-    {
-      name: 'ram 600 gb',
-      price: 50,
-    },
-  ];
-
-  public other: CartItem[] = [
-    {
-      name: 'potatoes',
-      price: 10,
-    }
-  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeViewComponent } from './views/home-view/home-view.component';
+import { ListViewComponent } from './views/list-view/list-view.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeViewComponent,
+    ListViewComponent
   ],
   imports: [
     BrowserModule
