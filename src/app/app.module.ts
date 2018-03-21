e60275c14c85e5d92633d87b6ff3a3c3a6cdfbdd
Szymon Rybka <szymon@MacBook-Pro-Szymon.local>
2018-03-21 20:25:16 +0100

    Added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
