d6fcd071fd1a37fd65b7fefee796a87bc51c03af
Szymon Rybka <szymon@MacBook-Pro-Szymon.local>
2018-03-21 19:43:17 +0100

    Part #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartItem } from './model/cart-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public telephones: CartItem[] = [
+    {
+      name: 'samsung s9+',
+      price: 300,
+    },
+    {
+      name: 'lg v30',
+      price: 301,
+    }
+  ];
 
+  public itStuff: CartItem[] = [
+    {
+      name: 'magic mouse',
+      price: 200,
+    },
+    {
+      name: 'ram 600 gb',
+      price: 50,
+    },
+  ];
+
+  public other: CartItem[] = [
+    {
+      name: 'potatoes',
+      price: 10,
+    }
+  ];
 }
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CartItem } from '../../model/cart-item';
 
 @Component({
@@ -7,38 +7,17 @@ import { CartItem } from '../../model/cart-item';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  @Input() public shopingList: CartItem[];
+  @Input() public listName: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  public shopingList: CartItem[] = [
-    {
-      name: "samsung s9+",
-      price: 300,
-    }, 
-    {
-      name: "lg v30",
-      price: 301,
-    }, 
-    {
-      name: "magic mouse",
-      price: 200,
-    }, 
-    {
-      name: "ram 600 gb",
-      price: 50,
-    }, 
-    {
-      name: "potatoes",
-      price: 10,
-    }
-  ];
-
   removeElement(element: CartItem): void {
     this.shopingList = this.shopingList.filter((item) => {
       return item !== element;
-    })
+    });
   }
 }
